ec9a32298dde95eed84ef8a1cc88a196a62bd44b
ascotlan <antonio.k.scotland@gmail.com>
2023-06-14 20:36:16 +0000

    establish connection in client.js and export connection object to play.js

--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
+const { connect } = require("./client");
 
 //listen for incoming data from the snake server
 connect().on("data", (data) => {
